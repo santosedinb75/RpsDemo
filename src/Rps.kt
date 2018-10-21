e5b699103d372138eb1cb4960a32760ff0229719
Mike Santos <santosedinb75@gmail.com>
2018-10-20 21:34:32 -0500

    refactor to show benefit of TDD

--- src/Rps.kt
+++ src/Rps.kt
@@ -3,38 +3,13 @@ import Winner.*
 
 class Rps {
     fun play(p1: Throw, p2: Throw): Winner {
-        if (p1 == Rock && p2 == Scissors) {
-            return P1_WINS
-        }
-
-        if (p1 == Rock && p2 == Paper) {
-            return P2_WINS
-        }
-
-        if (p1 == Scissors && p2 == Paper) {
-            return P1_WINS
-        }
-
-        if (p1 == Scissors && p2 == Rock) {
-            return P2_WINS
-        }
-
-        if (p1 == Paper && p2 == Scissors) {
-            return P2_WINS
-        }
-
-        if (p1 == Rock && p2 == Rock) {
+        if (p1 == p2)
             return TIE
-        }
 
-        if (p1 == Scissors && p2 == Scissors) {
-            return TIE
-        }
-
-        if (p1 == Paper && p2 == Paper) {
-            return TIE
-        }
-
+        if (p1 == Rock && p2 == Paper
+                || p1 == Scissors && p2 == Rock
+                || p1 == Paper && p2 == Scissors)
+            return P2_WINS
 
         return P1_WINS
     }
